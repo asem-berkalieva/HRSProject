506e3c9780497375e961878cb4d393f3fb078733
ee <espejo@berkeley.edu>
2019-11-20 14:41:40 -0800

    Update 01-data-exploration.R

--- code/espejo/01-data-exploration.R
+++ code/espejo/01-data-exploration.R
@@ -1,17 +1,18 @@
 source('functions/fwf-to-df.R')
 
 # ALL DATA FILES
-path_to_files <- '../../../../Dropbox/cal/classes/longitudinal/longitudinal-project/'
-data_files <- list.files(paste0(path_to_files,'2010-data'),
+path_to_files <- '../../../../Dropbox/cal/classes/longitudinal/longitudinal-project/download/'
+data_files <- list.files(paste0(path_to_files,'h10da'),
                          full.names=TRUE,
                          pattern='_R.da')
 
 # ALL CORRESPONDING DICTIONARIES
-dict_files <- gsub('data', 'stata', data_files)
-dict_files <- gsub('da',   'dct',   dict_files)
+dict_files <- gsub('da', 'sta', data_files)
+dict_files <- gsub('\\.sta',   '\\.dct',   dict_files)
 
 
-k <- 2
+# SEE THE KTH DATAFRAME
+k <- 1
 
 this_data <- data_files[k]
 this_dict <- dict_files[k]
